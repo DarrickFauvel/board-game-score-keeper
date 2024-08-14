df475f48296638ad468880ca646025c0e4270418
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-08-14 04:56:16 -0400

    Create game page

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,4 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const Games = () => {
-  return <div>List of games here...</div>
-}
-export default Games
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const res = await fetch("/api/games");
+      const data = await res.json();
+      setGames(data);
+    };
+
+    fetchGames();
+  }, []);
+
+  return (
+    <div>
+      List of games here...
+      <div>{games}</div>
+    </div>
+  );
+};
+export default Games;
